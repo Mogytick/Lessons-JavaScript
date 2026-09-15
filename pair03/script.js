f602921d3db3610d369f9c8bcb7cdcfbef5921aa
Mogytick <155806714+Mogytick@users.noreply.github.com>
2026-09-15 10:09:58 +0300

    Add files via upload

---
+++ pair03/script.js
@@ -0,0 +1,87 @@
+//1
+// let vik = +prompt("Введіть скільки вам років:")
+// if (vik <= 12){
+//     alert("Ви дитина")
+// }
+// else if (vik <= 18){
+//     alert('Ви підліток')
+// }
+// else{
+//     alert('Ви дорослий')
+// }
+//2;
+// const login = "admin";
+// const password = "12345";
+// let login1 = prompt("Введіть логін:");
+// let password1 = prompt("Введіть пароль:");
+// if (login === login1 && password === password1) {
+//     alert("Ви війшли");
+// }
+// else{
+//     alert("Ви не війшли");
+// }
+//3
+// let day = +prompt("Введдіть номер дня тижня");
+// if (!day < 1 && day > 7){
+//     switch(day){
+//         case 1:
+//             console.log("Понеділок");
+//             break;
+//         case 2:
+//             console.log("Вівторок");
+//             break;
+//         case 3:
+//             console.log("Середа");
+//             break;
+//         case 4:
+//             console.log("Четвер");
+//             break;
+//         case 5:
+//             console.log("П'ятниця");
+//             break;
+//         case 6:
+//             console.log("Субота");
+//             break;
+//         case 7:
+//             console.log("Неділя");
+//             break;
+//
+//     }
+// }
+//4---------------------------------------------------------
+// let product = prompt("Введи навзу товару:");
+// let price = +prompt("Введи ціну товару:");
+// let count = +prompt("Введи кількість товару:");
+// let discountCard = confirm("Чи є у вас дискотна карта??")
+// let dostavka = prompt("Введіть тип доставки:")
+// let allprice = price * count;
+// //<2k-5% 2<10% 5<-12% 10<15%
+// let shopDiscount = 0;
+// if(allprice >= 10000){
+//     shopDiscount = 15;
+// }
+// else if(allprice >= 5000){
+//     shopDiscount = 10;
+// }
+// else if (allprice >= 2000){
+//     shopDiscount = 5;
+// }
+// else{
+//     shopDiscount = 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
